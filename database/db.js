5febec4cc051e1108fe073b30156789798c45900
marquitocalderon <marco.calderon1701@hotmail.com>
2023-06-12 00:44:31 -0500

    axios la fdaj2

--- database/db.js
+++ database/db.js
@@ -1,21 +1,21 @@
 const mysql = require('mysql2'); // o 'mysql' si prefieres
 
 // Crea el pool de conexiones
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  port: 3306,
-  database: 'proyecto'
-});
-
 // const pool = mysql.createPool({
-//   host: 'buh5pev0wmpkxzzzunkj-mysql.services.clever-cloud.com',
-//   user: 'uxsvg0q0r58hniyu',
-//   password: 'BOHAFwBGKvil2t1JoGSO',
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
 //   port: 3306,
-//   database: 'buh5pev0wmpkxzzzunkj'
+//   database: 'proyecto'
 // });
+
+const pool = mysql.createPool({
+  host: 'buh5pev0wmpkxzzzunkj-mysql.services.clever-cloud.com',
+  user: 'uxsvg0q0r58hniyu',
+  password: 'BOHAFwBGKvil2t1JoGSO',
+  port: 3306,
+  database: 'buh5pev0wmpkxzzzunkj'
+});
 
 
 
